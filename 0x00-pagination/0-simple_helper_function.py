f7e779c49dac11eb6f63a78bfa4f30729860b12f
Pritchad25 <pricts29@gmail.com>
2024-01-29 21:47:31 +0000

    a function that takes two integer arguments page and page_size.

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Pagination Module"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ This function returns tuple of size two containing
+    a start index and an end index """
+    pass
