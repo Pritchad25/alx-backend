16ecf8c024ef9221a15d8434ed3b0dbd18eba603
Pritchad25 <pricts29@gmail.com>
2024-02-06 17:53:46 +0000

    making the file executable.

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -27,3 +27,8 @@ def index() -> str:
       - 1-index.html
     """
     return render_template('1-index.html')
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
